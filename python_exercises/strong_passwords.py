f554210d6f14da139f829ab9c003fcffcd0cf807
Morosi <hlonikgeleke@gmail.com>
2023-01-03 04:52:34 +0200

    added function to check for password strength

--- python_exercises/strong_passwords.py
+++ python_exercises/strong_passwords.py
@@ -2,19 +2,45 @@
 import datetime
 import time
 import random
+import string
 
-def Password():
-    lower_case = "abcdefghijklmnopqrstuvwxyz"
-    upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    number = "0123456789"
-    symbol = "!@#$%^&*()_+-=[]{}/<>;:|\?~`"
+# function that generates password string
+def generate_password():
+    password_chars = string.ascii_letters + string.digits + string.punctuation
+    password = "".join(random.choice(password_chars) for i in range(10))
+    return password
 
-    user_for = lower_case + upper_case + number + symbol
 
-    length_for_password = 10
-    password = "".join(random.sample(user_for,length_for_password))
-    print(f"Your Generated Password is:\n{password}\n")
+# function that checks the strength of the password string
 
-while 5 < 7:
-    Password()
+def check_password_strength(password):
+    strength = 0
+    common_passwords = ["password", "123456", "qwerty", "letmein", "monkey"]
+    if password in common_passwords:
+        return "This is too common and not strong enough"
+    if len(password) >= 8:
+        strength += 1
+    if any(char.isdigit() for char in password):
+        strength += 1
+    if any(char.islower() for char in password):
+        strength += 1
+    if any(char.isupper() for char in password):
+        strength += 1
+    if any(char in "!@#$%^&*()_+={}[]<>?/~`" for char in password):
+        strength += 1
+    if strength == 5:
+        print("This is a strong password\n")
+        return "This is a strong password"
+    if strength >= 3:
+        print("This password is medium strength\n")
+        return "This password is medium strength"
+    else:
+        print("You password is weak\n")
+        return "This is a weak password"
+
+while 5 > 3:
+    password = generate_password()
+    print(password)
+    check_password_strength(password)
     time.sleep(2)
+
