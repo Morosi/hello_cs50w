4ed5d0413272bd26fc514852a2afd5318bd26d22
Morosi <hlonikgeleke@gmail.com>
2023-01-23 03:02:57 +0200

    committing these changes because I think it is important to so

--- python_exercises/name.py
+++ python_exercises/name.py
@@ -1,3 +1,8 @@
 import sys
 
-print("hello, my name is", sys.argv[1])
+try:
+    print("hello, my name is", sys.argv[1])
+except IndexError:
+    name = input("enter your name: ")
+    print("")
+    print(f"hello, my name is {name}")
